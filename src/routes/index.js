532baa75cf0e0df0b589aa7bc83d527f2630e0ce
cassandra-mn <arthenciac@gmail.com>
2022-05-13 19:06:43 -0300

    return collections

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import productsRouter from './productsRouter.js';
+import collectionsRouter from './collectionsRouter.js';
 
 const router = express.Router();
 
 router.use(productsRouter);
+router.use(collectionsRouter);
 
 export default router;
