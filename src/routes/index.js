4f80a3e585aef42f529a905c57e22c18188df2c5
lorena morais <lorenamorais@alunos.utfpr.edu.br>
2022-05-12 23:24:45 -0300

    sign up page with validation and database

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import {register} from '../controllers/signUpController.js';
 
 const router = express.Router();
+router.post("/sign-up", register);
 
 export default router;
