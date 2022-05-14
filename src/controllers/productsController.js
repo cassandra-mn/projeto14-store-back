61c987bb5ac3f4b6aadde0198dbbd86435274d63
cassandra-mn <arthenciac@gmail.com>
2022-05-14 20:40:22 -0300

    return product

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -5,3 +5,10 @@ export async function products(req, res) {
 
     res.send(products);
 }
+
+export async function getProduct(req, res) {
+    const {id} = req.params;
+    const product = await db.collection('products').findOne({code: id});
+
+    res.status(200).send(product);
+}
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,9 +1,10 @@
 import express from 'express';
 
-import {products} from '../controllers/productsController.js';
+import {products, getProduct} from '../controllers/productsController.js';
 
 const productsRouter = express.Router();
 
 productsRouter.get('/products', products);
+productsRouter.get('/product/:id', getProduct);
 
 export default productsRouter;
