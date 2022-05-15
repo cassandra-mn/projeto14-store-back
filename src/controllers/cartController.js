dec03f504432f3d86be2f18270361e2dc1af273b
cassandra-mn <arthenciac@gmail.com>
2022-05-15 15:03:45 -0300

    add delete in cart

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -2,8 +2,10 @@ import db from '../db.js';
 
 export async function addProduct(req, res) {
     const product = req.body;
-
     try {
+        const exist = await db.collection('cart').findOne({code: product.code});
+        if (exist) return res.sendStatus(403);
+
         await db.collection('cart').insertOne(product);
         res.sendStatus(200);
     } catch(e) {
@@ -16,6 +18,21 @@ export async function cart(req, res) {
     try {
         const products = await db.collection('cart').find({}).toArray();
         res.send(products);
+    } catch(e) {
+        console.log(e);
+        res.status(500).send('Ocorreu um erro');
+    }
+}
+
+export async function exclude(req, res) {
+    const {id} = req.params;
+    
+    try {
+        const product = await db.collection('cart').findOne({code: id});
+        if (!product) return res.status(404).send('Produto não encontrado');
+        
+        await db.collection('cart').deleteOne({code: id});
+        res.sendStatus(200);
     } catch(e) {
         console.log(e);
         res.status(500).send('Ocorreu um erro');
--- src/routes/cartRouter.js
+++ src/routes/cartRouter.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import {addProduct, cart} from '../controllers/cartController.js';
+import {addProduct, cart, exclude} from '../controllers/cartController.js';
 
 const checkoutRouter = express.Router();
 
-checkoutRouter.post('/cart', addProduct);
 checkoutRouter.get('/cart', cart);
+checkoutRouter.post('/cart', addProduct);
+checkoutRouter.delete('/cart/:id', exclude);
 
 export default checkoutRouter;
