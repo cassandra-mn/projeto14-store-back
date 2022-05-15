0b2d428708023eabaa3b86282653116cb51d347a
cassandra-mn <arthenciac@gmail.com>
2022-05-15 09:51:16 -0300

    products router ok

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -8,7 +8,12 @@ export async function products(req, res) {
 
 export async function getProduct(req, res) {
     const {id} = req.params;
-    const product = await db.collection('products').findOne({code: id});
-
-    res.status(200).send(product);
+    
+    try {
+        const product = await db.collection('products').findOne({code: id});
+        res.send(product);
+    } catch(e) {
+        console.log(e);
+        res.status(500).send('Ocorreu um erro, tente novamente');
+    }
 }
