695646ca8a6265a3e5e36931387b64bde895ef91
cassandra-mn <arthenciac@gmail.com>
2022-05-15 10:54:18 -0300

    first commit

---
+++ src/routes/checkoutRouter.js
@@ -0,0 +1,5 @@
+import express from 'express';
+
+const checkoutRouter = express.Router();
+
+export default checkoutRouter;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 
 import productsRouter from './productsRouter.js';
 import collectionsRouter from './collectionsRouter.js';
+import checkoutRouter from './checkoutRouter.js';
 
 const router = express.Router();
 
 router.use(productsRouter);
 router.use(collectionsRouter);
+router.use(checkoutRouter);
 
 export default router;
